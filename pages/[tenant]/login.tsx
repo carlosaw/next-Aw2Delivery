23354f4ee1ac9fb2b7dd1d2b83819310df9b529a
carlosaw <carlosfreevg@gmail.com>
2023-01-12 21:45:28 -0400

    SignIn/Signup

--- pages/[tenant]/login.tsx
+++ pages/[tenant]/login.tsx
@@ -1,6 +1,9 @@
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import  Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { InputField } from '../../components/InputField';
 import { useAppContext } from '../../contexts/AppContext';
@@ -10,12 +13,22 @@ import { Tenant } from '../../types/Tenant';
 
 const Login = (data: Props) => {
   const { tenant, setTenant } = useAppContext();
-  useEffect(()=>{
+  useEffect(() => {
     setTenant(data.tenant);
   }, []);
 
+  const router = useRouter();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  function handleSubmit(): void {
+    
+  }
+
+  function handleSignUp(): void {
+    router.push(`/${data.tenant.slug}/signup`);
+  }
 
   return (
     <div className={styles.container}>
@@ -25,28 +38,70 @@ const Login = (data: Props) => {
 
       <Header color={data.tenant.mainColor} backHref={`/${data.tenant.slug}`} />
 
-      <InputField 
-        color={data.tenant.mainColor}
-        placeholder="Digite seu e-mail"
-        value={email}
-        onChange={setEmail}
-      />
-      <InputField 
-        color={data.tenant.mainColor}
-        placeholder="Digite sua senha"
-        value={password}
-        onChange={setPassword}
-        password
-      />
+      <div className={styles.header}>{data.tenant.name}</div>
+
+      <div
+        className={styles.subtitle}
+        style={{ borderBottomColor: data.tenant.mainColor }}
+        >Use suas credenciais para realizar o login.</div>
+
+      <div className={styles.line}></div>
+
+      <div className={styles.formArea}>
+        <div className={styles.inputArea}>
+          <InputField
+            color={data.tenant.mainColor}
+            placeholder="Digite seu e-mail"
+            value={email}
+            onChange={setEmail}
+          />
+        </div>
+        <div className={styles.inputArea}>
+          <InputField
+            color={data.tenant.mainColor}
+            placeholder="Digite sua senha"
+            value={password}
+            onChange={setPassword}
+            password
+          />
+        </div>
+        <div className={styles.inputArea}>
+          <Button
+            color={data.tenant.mainColor}
+            label="Entrar"
+            onClick={handleSubmit}
+            fill
+          />
+        </div>
+      </div>
+
+      <div
+        className={styles.forgetArea}
+        style={{ borderBottomColor: data.tenant.mainColor }}
+      >
+        Esqueceu sua senha? <Link style={{ color: data.tenant.mainColor }} href={`/${data.tenant.slug}/forget`}>Clique aqui!</Link>
+      </div>
+
+      <div className={styles.line}></div>
+
+      <div className={styles.signupArea}>
+        <Button
+          color={data.tenant.mainColor}
+          label="Quero me cadastrar"
+          onClick={handleSignUp}
+          fill={false}
+        />
+      </div>
+
     </div>
-  )
+  );
 }
 
 export default Login;
 
 // Validação do slug
 type Props = {
-  tenant: Tenant; 
+  tenant: Tenant;
 }
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { tenant: tenantSlug } = context.query;
@@ -55,8 +110,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   // Get Tenant (identificando o tenant)
   const tenant = await api.getTenant(tenantSlug as string);
-  if(!tenant) {
-    return { redirect: { destination: '/', permanent: false } }   
+  if (!tenant) {
+    return { redirect: { destination: '/', permanent: false } }
   }
 
   return {
